cd4271aeded7997937477fac1d8ae4708a75f2c7
Adam <adambrikman@gmail.com>
2020-01-22 15:59:54 -0800

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ const app = express();
 const cors = require("cors");
 const path = require("path");
 
-const port = process.env.PORT || 5000;
-
 // Middleware
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
 // Connect to Mongoose
 const mongoose = require("mongoose");
@@ -33,15 +31,11 @@ app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
-}
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
