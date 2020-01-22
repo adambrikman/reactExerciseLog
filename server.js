15eceaeed2b286277925f22fdd8bc684b3a4b8da
Adam <adambrikman@gmail.com>
2020-01-22 15:19:53 -0800

    Update server.js for deployment

--- server.js
+++ server.js
@@ -1,40 +1,44 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const path = require('path');
-
-require("dotenv").config();
-
 const app = express();
+const cors = require("cors");
+const path = require("path");
+
 const port = process.env.PORT || 5000;
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, {
+// Connect to Mongoose
+const mongoose = require("mongoose");
+mongoose.connect(process.env.ATLAS_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
 });
 
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("Connected to MongoDB");
-});
+const db = mongoose.connection;
+db.on("error", err => console.error(err));
+db.once("open", () => console.log("Connected to Mongoose"));
 
 const exercisesRouter = require("./routes/exercises");
 const usersRouter = require("./routes/users");
 
+// Utilize routes
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
+
   // Set static folder
   app.use(express.static('client/build'));
 
-  app.length('*', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   })
 }
