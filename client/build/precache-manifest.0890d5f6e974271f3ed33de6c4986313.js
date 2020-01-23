2d4ca343d704ec2b005afcbb35b58c3370a75fce
Adam <adambrikman@gmail.com>
2020-01-22 16:08:51 -0800

    Update script for deployment

---
+++ client/build/precache-manifest.0890d5f6e974271f3ed33de6c4986313.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82712a18c8e13c90b22c67fcee80312",
+    "url": "/index.html"
+  },
+  {
+    "revision": "030b253a14b6a5491193",
+    "url": "/static/css/2.ce2441d0.chunk.css"
+  },
+  {
+    "revision": "f5def05841e462712b42",
+    "url": "/static/css/main.6259b895.chunk.css"
+  },
+  {
+    "revision": "030b253a14b6a5491193",
+    "url": "/static/js/2.e8187006.chunk.js"
+  },
+  {
+    "revision": "4c3b865ea8d319faf1c56c47d6e1060e",
+    "url": "/static/js/2.e8187006.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5def05841e462712b42",
+    "url": "/static/js/main.c8b7d81b.chunk.js"
+  },
+  {
+    "revision": "1809a694a46bf86e0e94",
+    "url": "/static/js/runtime-main.24340f72.js"
+  }
+]);
