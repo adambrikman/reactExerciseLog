b5d1782a5bcc25524b45c963d5980a9ea9586f1a
Adam <adambrikman@gmail.com>
2020-01-22 16:40:32 -0800

    Updated servers.js

--- server.js
+++ server.js
@@ -31,9 +31,15 @@ app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 
 // Serve static assets if in production
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+if (process.env.NODE_ENV === 'production') {
+
+  app.use(express.static('client/build'));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 
 const port = process.env.PORT || 5000;
 
