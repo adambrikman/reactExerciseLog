d1248fe012fa5971c73b3e827fb0cc45629d8327
Adam <adambrikman@gmail.com>
2020-01-23 13:13:14 -0800

    update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-// if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
-// }
+}
 
 const express = require("express");
 const app = express();
@@ -29,9 +29,16 @@ const usersRouter = require("./routes/users");
 // Utilize routes
 app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-});
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
