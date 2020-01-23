175da8bbc8c7247d14d41b9c3e0726d281c87855
Adam <adambrikman@gmail.com>
2020-01-23 09:45:10 -0800

    updated package.json

--- server.js
+++ server.js
@@ -31,9 +31,9 @@ app.use("/exercises", exercisesRouter);
 app.use("/users", usersRouter);
 
 // Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
 
-  app.use('/', express.static(path.join(__dirname, '/client/build')));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
